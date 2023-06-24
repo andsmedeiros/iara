c00277728902d4f7f44224b5fca211d495b6e335
André Medeiros <andsmedeiros@gmail.com>
2023-06-23 21:55:28 -0300

    Main fugax header file

---
+++ fugax/include/fugax.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file fugax/include/fugax.hpp
+ * @brief Main include header for Fugax
+ * @author kazeshi
+ * @date 22/06/23
+ * @copyright (C) 2023 kazeshi
+**/
+
+#ifndef FUGAX_HPP
+#define FUGAX_HPP
+
+#include "fugax/event-loop.hpp"
+
+#endif /* FUGAX_HPP */
